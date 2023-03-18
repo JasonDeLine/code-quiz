0ad3d18232cb32374c7e892b6626a47aaf77215d
Jason DeLine <jcdeline1982@gmail.com>
2023-03-18 09:31:58 -0500

    updating file structure, starting first JS file wiht questions

---
+++ assets/JS/codequestions.js
@@ -0,0 +1,8 @@
+// creating one sheet for all questions
+var allQuestions = [
+  {
+    question: "In a FOR loop what does the letter I stand for?",
+    selection: ["Indicate", "Indicator", "Index", "Initiate"],
+    answer: "Index",
+  },
+];
