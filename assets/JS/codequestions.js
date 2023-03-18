7a9d9611be163589aeadfb180b5e78dc4972b917
Jason DeLine <jcdeline1982@gmail.com>
2023-03-18 09:51:17 -0500

    adding initial JS questions

--- assets/JS/codequestions.js
+++ assets/JS/codequestions.js
@@ -5,4 +5,34 @@ var allQuestions = [
     selection: ["Indicate", "Indicator", "Index", "Initiate"],
     answer: "Index",
   },
+
+  {
+    question:
+      "In order to link an external JavaScript file, to an HTML file, you must add a <script> tag to you JavaScript file.",
+    selection: ["True", "False"],
+    answer: ["False. You must add the <script> tag to your HTML file."],
+  },
+
+  {
+    question:
+      "An array may NOT contain which of the following types of elements?",
+    selection: ["Strings", "Boolean", "Numbers", "./assets/JS/script.js"],
+    answer: ["./assets/JS/codequestions.js"],
+  },
+
+  {
+    question: "Of the following, which is the correct line of an IF statement?",
+    selection: [
+      "if (hour < 18) {",
+      "if hour < 18 {",
+      "if (hour is less than 18)",
+      "if is = hour less than 18",
+    ],
+    answer: ["if (hour < 18) {"],
+  },
+
+  question: "What is an example of code that is commented out?",
+  selection: ["// This code is commented out", "/This code is commented out", "**This code is commented out", "##This code is commented out"],
+  answer: ["//This code is commented out"],
 ];
+// This code is commented out
